3ce8bb8d399546ca737ad4c873ecd2a329b7e937
Leon Pearce <leonp1991@gmail.com>
2018-01-03 12:31:13 +1000

    ADD: initial app without any react

---
+++ source/View/index.jsx
@@ -0,0 +1,7 @@
+
+
+// we will turn this into the root of our react view
+export default function DefinitelyNotAReactView() {
+  // try changing this to test hot reloading
+  document.body.innerHTML = 'HELLO WORLD'
+}
