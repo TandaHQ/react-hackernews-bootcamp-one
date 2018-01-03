4207769a5140f171b197fd8de061d727a2db14eb
Leon Pearce <leonp1991@gmail.com>
2018-01-03 14:05:11 +1000

    ADD: initial styles to hello world

--- source/View/index.jsx
+++ source/View/index.jsx
@@ -1,7 +1,13 @@
+import styles from "./styles.css"
 
 
 // we will turn this into the root of our react view
 export default function DefinitelyNotAReactView() {
   // try changing this to test hot reloading
-  document.body.innerHTML = 'HELLO WORLD'
+  document.body.innerHTML =
+    `
+      <div class=${styles.helloWorld}>
+        Hello Hacker News
+      </div>
+    `
 }
