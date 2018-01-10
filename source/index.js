19aac26498f71b8f2400731ff378250d446ccb3f
Leon Pearce <leonp1991@gmail.com>
2018-01-11 09:39:08 +1000

    ADD: our new article component

--- source/index.js
+++ source/index.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'Hello Hacker News';
 
-const element =
-  <div
-    style={{ fontSize: 36, color: 'green' }}
-    onClick={() => window.alert(title)}
-  >
-    {title}
-  </div>
+/**
+ * our first component, accepts 2 props:
+ * - title (of article)
+ * - link (to article)
+ * - description (of article)
+ */
+function Article(props) {
+  return (
+    <article>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <a href={props.link}>{props.link}</a>
+    </article>
+  )
+}
 
-const root = document.getElementById('root');
 
-ReactDOM.render(element, root);
+// random description
+const LOREM_IPSUM =
+  `
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the
+  1500s, when an unknown printer took a galley of type and scrambled it to
+  make a type specimen book.
+  `
+
+
+// our static hacker new feed
+const VIEW =
+  <Article
+    description={LOREM_IPSUM}
+    link="google.com/foobar"
+    title="Article 1"
+  />
+
+
+ReactDOM.render(VIEW, document.getElementById('root'));
